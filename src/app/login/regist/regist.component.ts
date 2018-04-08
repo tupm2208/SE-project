ad3960538a61ba7275c24ba63cb9347533b43f9d
Pham Quang Huy <p.q.huy97@gmail.com>
2018-04-08 23:43:44 +0700

    validate register informaton

--- src/app/login/regist/regist.component.ts
+++ src/app/login/regist/regist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-regist',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../login.component.css']
 })
 export class RegistComponent implements OnInit {
+  
+  private fullname: String;
+  private email: String;
+  private username: String;
+  private password: String;
+  private confirmPassword: String;
 
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  submit() {
+    
   }
 
 }
