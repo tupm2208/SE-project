005df8608cf19dc3a9d3af056c53b2f3efb197d5
Pham Quang Huy <p.q.huy97@gmail.com>
2018-05-02 20:16:03 +0700

    add new comment

--- src/app/main/post/comment/comment-detail/comment-detail.component.ts
+++ src/app/main/post/comment/comment-detail/comment-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormatService } from '../../../../core/util/format.service';
+import { StorageService } from '../../../../core/util/storage.service';
+declare var $:any;
 @Component({
   selector: 'app-comment-detail',
   templateUrl: './comment-detail.component.html',
@@ -8,12 +10,17 @@ import { FormatService } from '../../../../core/util/format.service';
 export class CommentDetailComponent implements OnInit {
   @Input() comment: any;
   
+  private isCommentator: Boolean = false;
+
   constructor(
     private formatService: FormatService,
-  ) { }
+    private storageService: StorageService,
+  ) { 
+    
+  } 
 
   ngOnInit() {
-    
+    this.isCommentator = (this.comment.authorID == this.storageService.get("id")) ? true : false;
   }
 
 }
--- src/app/main/post/comment/comment.component.ts
+++ src/app/main/post/comment/comment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { StorageService } from '../../../core/util/storage.service';
 import { FormatService } from '../../../core/util/format.service';
+import { CommentService } from '../../../core/api/comment.service';
 declare var $: any;
 
 @Component({
@@ -16,11 +17,14 @@ export class CommentComponent implements OnInit {
   private content: String;
   private isAuthor: Boolean;
   private user: any;
+  private isLogin: Boolean = false;
   
   constructor(
     private storageService: StorageService,
     private formatService: FormatService,
+    private commentService: CommentService,
   ) { 
+    this.isLogin = this.storageService.get("token") ? true : false;
   }
 
   ngOnInit() {
@@ -28,13 +32,26 @@ export class CommentComponent implements OnInit {
   }
 
   submit() {
+
+    if (!this.content) return;
+
     var newComment = {
-      commentator: this.getUserData(),
       content: this.content,
-      createdAt: Date.now()
-    }   
-  
-    this.comments.unshift(newComment);
+      postID: this.postDetail.ID,
+    }
+    // get User information as commentator
+    var commentator = this.getUserData();
+    
+    this.commentService.post(newComment).subscribe(dataReturn => {
+      // add commentator attribute and bind information to comments array
+      dataReturn.data.commentator = commentator;
+      this.comments.unshift(dataReturn.data);
+     
+    }, err => {
+      alert("Something wrong happen!!!");
+    })
+    
+    
   }
 
   getUserData() {
