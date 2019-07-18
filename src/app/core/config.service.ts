5e7241b02d52531c8ba433a8f3b6aeb1295f9fb0
trachpro <tu.phamminh.2207@gmail.com>
2019-07-18 22:34:00 +0700

    finish

--- src/app/core/config.service.ts
+++ src/app/core/config.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigService {
 
-  private urlBase = 'https://app-4a5dbc4f-d024-436d-a4c4-74ab131c9fa3.cleverapps.io/api/'
+  private urlBase = 'https://fast-springs-91413.herokuapp.com/api/'
   constructor() { }
 
   getBaseURL() {
