eaf0e3efe5e15b5188ba35453a1f37cfda828416
trachpro <tu.phamminh.2207@gmail.com>
2019-07-19 21:49:46 +0700

    finish

--- src/app/core/util/storage.service.ts
+++ src/app/core/util/storage.service.ts
@@ -7,17 +7,17 @@ export class StorageService {
 
   set(key, value) {
 
-    sessionStorage.setItem(key, JSON.stringify(value));
+    localStorage.setItem(key, JSON.stringify(value));
   }
 
   get(key) {
 
-    return JSON.parse(sessionStorage.getItem(key));
+    return JSON.parse(localStorage.getItem(key));
   }
 
   public removeItem(key: string): void {
     
-    delete sessionStorage[key];
+    delete localStorage[key];
   }
 
 }
